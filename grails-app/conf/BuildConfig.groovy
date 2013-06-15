ebc7c91f7db0a86702aeeaf0b1e7da6c93db0856
Hideki IGARASHI <hideki.develop@gmail.com>
2013-06-15 17:10:17 +0900

    Install Spock Plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,8 +40,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.22'
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
@@ -59,5 +58,9 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
+
+        test(":spock:0.7") {
+            exclude "spock-grails-support"
+        }
     }
 }
